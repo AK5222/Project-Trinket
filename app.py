1fac5252eec31f81d8b4d982ad909c0a3356715d
jamesglassford15 <jamesglassford15@gmail.com>
2025-02-11 04:10:38 -0800

    added some test supabase stuff

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
+from supabase import create_client, Client
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv('SUPABASE_KEY')
+supabase: Client = create_client(url, key)
 
 #reference file name
 app = Flask(__name__)
@@ -29,4 +37,6 @@ def list():
 
 
 if __name__ == "__main__":
+    print(url)
+    print(key)
     app.run(debug=True)
